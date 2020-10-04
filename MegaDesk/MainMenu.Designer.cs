247663a7913afcdea26932daa1ec4d936c8d4cd5
cvr-byui <cvanry@byui.edu>
2020-10-03 19:50:31 -0600

    MegaDesk Forms Created

---
+++ MegaDesk/MainMenu.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MegaDesk
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddQuoteButton = new System.Windows.Forms.Button();
+            this.ViewQuotesButton = new System.Windows.Forms.Button();
+            this.SearchQuotesButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddQuoteButton
+            // 
+            this.AddQuoteButton.Location = new System.Drawing.Point(61, 102);
+            this.AddQuoteButton.Name = "AddQuoteButton";
+            this.AddQuoteButton.Size = new System.Drawing.Size(130, 23);
+            this.AddQuoteButton.TabIndex = 0;
+            this.AddQuoteButton.Text = "&Add New Quote";
+            this.AddQuoteButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewQuotesButton
+            // 
+            this.ViewQuotesButton.Location = new System.Drawing.Point(61, 167);
+            this.ViewQuotesButton.Name = "ViewQuotesButton";
+            this.ViewQuotesButton.Size = new System.Drawing.Size(130, 23);
+            this.ViewQuotesButton.TabIndex = 1;
+            this.ViewQuotesButton.Text = "&View Quotes";
+            this.ViewQuotesButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchQuotesButton
+            // 
+            this.SearchQuotesButton.Location = new System.Drawing.Point(61, 239);
+            this.SearchQuotesButton.Name = "SearchQuotesButton";
+            this.SearchQuotesButton.Size = new System.Drawing.Size(130, 23);
+            this.SearchQuotesButton.TabIndex = 2;
+            this.SearchQuotesButton.Text = "&Search Quotes";
+            this.SearchQuotesButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(61, 318);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(130, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.SearchQuotesButton);
+            this.Controls.Add(this.ViewQuotesButton);
+            this.Controls.Add(this.AddQuoteButton);
+            this.Name = "MainMenu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddQuoteButton;
+        private System.Windows.Forms.Button ViewQuotesButton;
+        private System.Windows.Forms.Button SearchQuotesButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
